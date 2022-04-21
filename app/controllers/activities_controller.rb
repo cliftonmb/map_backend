4a2aa24f81366a7a4f69d48e4a3440cc853ac666
Clifton Butler <cliftonmbutler@gmail.com>
2022-04-21 14:17:33 -0500

    add index action

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,8 @@
 class ActivitiesController < ApplicationController
+
+  def index
+    activity = Activity.first
+    render json: activity.latitude
+  end
+
 end
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,2 +1,7 @@
 class Activity < ApplicationRecord
+
+  def latitude
+    return activity.name
+  end
+
 end
